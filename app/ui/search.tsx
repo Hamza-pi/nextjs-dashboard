4ff7861168d4b21b7ce8f8dbc0ffba6698e55647
Hamza-pi <mukhtarhamza294@gmail.com>
2024-09-23 22:12:06 +0500

    completed till chapter 12

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,40 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { ChangeEvent } from "react";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
-    console.log(term);
+  function debounce<T>(
+    this: T,
+    func: (this: T, ...args: any[]) => void,
+    delay: number
+  ) {
+    let timeoutId: NodeJS.Timeout;
+    return function (this: T, ...args: any[]) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func.call(this, ...args);
+      }, delay);
+    };
+  }
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const term = e.target.value;
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (term) params.set("query", term);
+    else params.delete("query");
+    replace(`${pathname}?${params.toString()}`);
   };
+
+  const decoratedSearch = debounce(handleSearch, 1500);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -18,7 +44,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={decoratedSearch}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
